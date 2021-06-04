de129526fed8264ebfcfb4b62d8833bf614a94ac
Jiahao Li <lijiahao_shandong@163.com>
2021-06-04 15:00:32 +0800

    feat: remove router

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,12 @@
 import { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loading from '@/components/loading';
-import routes from '@/routes';
+import Home from '@/pages/home';
 
 const App = () => (
   <Suspense fallback={<Loading />}>
-    <Router>
-      <main className="container bg-body">
-        <Routes>
-          {routes.map(({ path, element: Page }) => (
-            <Route key={path} path={path} element={<Page />} />
-          ))}
-        </Routes>
-      </main>
-    </Router>
+    <main className="container bg-body">
+      <Home />
+    </main>
   </Suspense>
 );
 
